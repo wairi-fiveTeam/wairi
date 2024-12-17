1c23ba92752bde625fa60ab7c7211e892ac469fa
jrary <jrary@naver.com>
2024-12-18 04:07:19 +0900

    Feat: 인플루언서 모달창 디자인

--- src/app/member/first/FirstSection.tsx
+++ src/app/member/first/FirstSection.tsx
@@ -2,25 +2,25 @@ import Image from 'next/image';
 
 export default function FirstSection() {
   return (
-    <header className='relative py-16'>
+    <header className="relative py-16 bg-[#C3F6F6] bg-[url('../../public/images/background_bubble.png')] bg-repeat">
       <div className='max-w-[1440px] mx-auto'>
         {/* 헤더 왼쪽 */}
         <div className='flex items-center space-x-8'>
           <div>
-            <h1 className='text-2xl font-semibold'>인플루언서 마케팅 NO.1</h1>
+            <h1 className='text-2xl'>인플루언서 마케팅 NO.1</h1>
             <h2 className='text-5xl font-bold mt-2 leading-tight'>
               여행 인플루언서
               <br />
               마케팅의 시작, 와이리
             </h2>
-            <div className='mt-8 space-x-4'>
-              <div className='flex items-center'>
-                <div className='w-[6rem] h-[6.5rem] bg-white rounded-full items-center justify-center shadow-lg'>
-                  <Image src='/images/qr_appstore.png' alt='QR Code for App Store' width={100} height={100} />
+            <div className='mt-20 space-x-4'>
+              <div className='flex items-center gap-6'>
+                <div className='w-36 h-44 bg-white rounded-3xl shadow-xl flex flex-col items-center justify-center'>
+                  <Image src='/images/qr_appstore.png' alt='QR Code for App Store' width={110} height={110} />
                   <p className='text-sm mt-2'>App Store</p>
                 </div>
-                <div className='w-[6rem] h-[6.5rem] bg-white rounded-full items-center justify-center shadow-lg'>
-                  <Image src='/images/qr_playstore.png' alt='QR Code for Google Play' width={100} height={100} />
+                <div className='w-36 h-44 bg-white rounded-3xl shadow-xl flex flex-col items-center justify-center'>
+                  <Image src='/images/qr_playstore.png' alt='QR Code for Google Play' width={110} height={110} />
                   <p className='text-sm mt-2'>Google Play</p>
                 </div>
               </div>
--- src/app/member/first/MemberModal.tsx
+++ src/app/member/first/MemberModal.tsx
@@ -2,10 +2,11 @@
 import Image from 'next/image';
 
 interface ModalProps {
+  isSmallScreen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function MemberModal({ setIsOpen }: ModalProps) {
+export default function MemberModal({ isSmallScreen, setIsOpen }: ModalProps) {
   const handleModalClose = () => {
     setIsOpen(false);
   };
@@ -20,22 +21,81 @@ export default function MemberModal({ setIsOpen }: ModalProps) {
           e.stopPropagation();
         }}
       >
-        <Image
-          src={'/images/popup_web.svg'}
-          alt='테스트'
-          width={0}
-          height={0}
-          sizes='100vw'
-          style={{ width: '40vw', height: 'auto' }}
-        />
-        <Image
-          src={'/images/ic_close.svg'}
-          alt='닫기 버튼'
-          width={50}
-          height={50}
-          className='absolute top-5 right-5 cursor-pointer'
-          onClick={handleModalClose}
-        />
+        {isSmallScreen ? (
+          <div>
+            <Image
+              src={'/images/popup_mobile.svg'}
+              alt='배경 이미지'
+              width={0}
+              height={0}
+              sizes='80vw'
+              style={{
+                width: '80vw',
+                height: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                position: 'relative',
+                paddingBottom: '20px',
+              }}
+            />
+            <Image
+              src={'/images/ic_close.svg'}
+              alt='닫기 버튼'
+              width={40}
+              height={40}
+              className='absolute top-5 right-5 cursor-pointer'
+              onClick={handleModalClose}
+            />
+            <div className='absolute w-3/4 bottom-[20%] left-1/2 transform -translate-x-1/2'>
+              <Image
+                src={'/images/popup_apple_btn.svg'}
+                alt='애플 앱스토어 다운로드'
+                width={0}
+                height={0}
+                sizes='100vw'
+                className='cursor-pointer'
+                style={{
+                  width: '100vw',
+                  height: 'auto',
+                  marginBottom: '16px',
+                }}
+              />
+              <Image
+                src={'/images/popup_gp_btn.svg'}
+                alt='구글 플레이 다운로드'
+                width={0}
+                height={0}
+                sizes='100vw'
+                className='cursor-pointer'
+                style={{
+                  width: '100vw',
+                  height: 'auto',
+                }}
+              />
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Image
+              src={'/images/popup_web.svg'}
+              alt='테스트'
+              width={0}
+              height={0}
+              sizes='100vw'
+              style={{ width: '40vw', minWidth: '700px', height: 'auto' }}
+            />
+            <Image
+              src={'/images/ic_close.svg'}
+              alt='닫기 버튼'
+              width={50}
+              height={50}
+              className='absolute top-5 right-5 cursor-pointer'
+              onClick={handleModalClose}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
