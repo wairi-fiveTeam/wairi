c5d21ec363c89a0988462450ba7decfcd9653edd
박초롱 <rong9835@naver.com>
2024-12-18 14:34:51 +0900

    feat : 푸터 작성

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,33 +3,43 @@ import wairiLogo from '../../public/images/wairi_logo.svg';
 
 export default function Footer() {
   return (
-    <footer className='bg-[#424242] w-full py-14 max-md:py-8'>
-      <div className='max-w-[1440px] mx-auto px-[100px] max-md:px-5'>
+    <footer className='bg-[#424242] w-full py-10 max-md:py-8'>
+      <div className='max-w-[860px] mx-auto px-[100px] max-md:px-5'>
         {/* 로고 + 회사명 섹션 */}
-        <div className='flex items-center mb-2'>
-          <Image
-            src={wairiLogo}
-            alt='와이리로고'
-            width={135}
-            height={62}
-            className='mr-[24px] max-md:w-[77px] max-md:h-[34px]'
-          />
-          <span className='text-[28px] text-white max-md:text-[16px]'>(주)와이리</span>
+        <div className='flex justify-between'>
+          <div className='flex items-center mb-2'>
+            <Image
+              src={wairiLogo}
+              alt='와이리로고'
+              width={80}
+              height={30}
+              className='mr-[24px] max-md:w-[77px] max-md:h-[34px]'
+            />
+            <span className='text-[18px] text-white max-md:text-[16px]'>(주)와이리</span>
+          </div>
+          <a
+            href='https://2k044.channel.io/user-chats/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hidden max-md:block'
+          >
+            <Image src='/images/chat.svg' alt='챗봇연결' width={60} height={60} />
+          </a>
         </div>
         {/* 회사정보 + 고객센터 컨테이너 */}
-        <div className='flex gap-[133px] max-md:flex-col max-md:gap-[29px]'>
+        <div className='flex gap-[50px] max-md:flex-col max-md:gap-[29px]'>
           {/* 회사정보 */}
-          <div className='max-md:hidden text-[25px] leading-[40px] text-[#9E9E9E]'>
+          <div className='max-md:hidden text-[14px] text-[#9E9E9E]'>
             <div>대표자 : 김범석</div>
             <div>사업자등록번호 : 411-86-02504 | 통신판매업신고 : 2021-서울영등포-2197</div>
             <div>주소 : 서울시 동작구 상도로 55길 6, 테크스테이션 408호</div>
             <div>이메일 : wairi_sales@naver.com | 대표전화 : 070-8098-7127</div>
           </div>
           {/* 고객센터 정보 */}
-          <div className='max-md:hidden text-white leading-[42px]'>
-            <div className='text-[28px]'>와이리 고객센터</div>
-            <div className='text-[35px] font-bold'>070-8098-7127</div>
-            <div className='text-[28px]'>운영시간 평일 10:00~18:00</div>
+          <div className='max-md:hidden text-white'>
+            <div className='text-[14px]'>와이리 고객센터</div>
+            <div className='text-[18px] font-bold'>070-8098-7127</div>
+            <div className='text-[14px]'>운영시간 평일 10:00~18:00</div>
           </div>
 
           {/* 모바일 레이아웃 */}
